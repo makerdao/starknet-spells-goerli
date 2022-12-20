c78f70e4fd89ce472e95be7824de7d7e7aeeaa13
Maciek Kamiński <aundumla@gmail.com>
2022-12-20 18:15:55 +0100

    call helper

--- test/all.test.ts
+++ test/all.test.ts
@@ -9,66 +9,66 @@ import {
   L2_GOVERNANCE_RELAY_ADDRESS,
   L2_GOVERNANCE_RELAY_LEGACY_ADDRESS,
 } from "./addresses";
-import { getL2ContractAt, isWard } from "./utils";
+import { getL2ContractAt, isWard, wrap } from "./utils";
 
 export const USER =
   "0x063c94d6B73eA2284338f464f86F33E12642149F763Cd8E76E035E8E6A5Bb0e6";
 
 describe("setup", () => {
-  let dai: StarknetContract;
-  let bridge: StarknetContract;
-  let bridgeLegacy: StarknetContract;
-  let teleport: StarknetContract;
-  let relay: StarknetContract;
-  let relayLegacy: StarknetContract;
+  let dai: any;
+  let bridge: any;
+  let bridgeLegacy: any;
+  let teleport: any;
+  let relay: any;
+  let relayLegacy: any;
 
   before(async () => {
-    dai = await getL2ContractAt(hre, "test/abis/dai_abi.json", L2_DAI_ADDRESS);
-    bridge = await getL2ContractAt(
+    dai = wrap(await getL2ContractAt(hre, "test/abis/dai_abi.json", L2_DAI_ADDRESS));
+    bridge =  wrap(await getL2ContractAt(
       hre,
       "test/abis/l2_dai_bridge_abi.json",
       L2_DAI_BRIDGE_ADDRESS
-    );
-    bridgeLegacy = await getL2ContractAt(
+    ));
+    bridgeLegacy =  wrap(await getL2ContractAt(
       hre,
       "test/abis/l2_dai_bridge_abi.json",
       L2_DAI_BRIDGE_LEGACY_ADDRESS
-    );
-    teleport = await getL2ContractAt(
+    ));
+    teleport = wrap(await getL2ContractAt(
       hre,
       "test/abis/l2_dai_teleport_gateway_abi.json",
       L2_DAI_TELEPORT_GATEWAY_ADDRESS
-    );
-    relay = await getL2ContractAt(
+    ));
+    relay = wrap(await getL2ContractAt(
       hre,
       "test/abis/l2_governance_relay_abi.json",
       L2_GOVERNANCE_RELAY_ADDRESS
-    );
-    relayLegacy = await getL2ContractAt(
+    ));
+    relayLegacy = wrap(await getL2ContractAt(
       hre,
       "test/abis/l2_governance_relay_abi.json",
       L2_GOVERNANCE_RELAY_LEGACY_ADDRESS
-    );
+    ));
   });
 
   describe("dai", () => {
     it("authorizations", async () => {
-      expect(await isWard(dai, bridge)).toBeTruthy();
-      expect(await isWard(dai, relay)).toBeTruthy();
-      expect(await isWard(dai, bridgeLegacy)).toBeTruthy();
-      expect(await isWard(dai, relayLegacy)).toBeTruthy();
+      expect(await dai.wards(bridge.address)).toBeTruthy();
+      expect(await dai.wards(relay.address)).toBeTruthy();
+      expect(await dai.wards(bridgeLegacy.address)).toBeTruthy();
+      expect(await dai.wards(relayLegacy.address)).toBeTruthy();
     });
   });
   describe("bridge", () => {
     it("authorizations", async () => {
-      expect(await isWard(bridge, relay)).toBeTruthy();
-      expect(await isWard(bridge, relayLegacy)).toBeTruthy();
+      expect(await bridge.wards(relay.address)).toBeTruthy();
+      expect(await bridge.wards(relayLegacy.address)).toBeTruthy();
     });
   });
   describe("teleport", () => {
     it("authorizations", async () => {
-      expect(await isWard(teleport, relay)).toBeTruthy();
-      expect(await isWard(teleport, relayLegacy)).toBeTruthy();
+      expect(await teleport.wards(relay.address)).toBeTruthy();
+      expect(await teleport.wards(relayLegacy.address)).toBeTruthy();
     });
   });
 });
--- test/utils.ts
+++ test/utils.ts
@@ -1,6 +1,8 @@
+import { AbiEntry } from "@shardlabs/starknet-hardhat-plugin/dist/src/starknet-types";
 import { StarknetContractFactory } from "@shardlabs/starknet-hardhat-plugin/dist/src/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import path from "path";
+import { zip} from "lodash";
 
 export async function getL2ContractAt(
   hre: HardhatRuntimeEnvironment,
@@ -21,4 +23,48 @@ export async function isWard(
 ): Promise<boolean> {
   const { res } = await hasWards.call("wards", { user: ward.address });
   return res === 1n;
+}
+
+function getCallArgs(inputs, args) {
+  return zip(inputs, args).reduce(
+    (result, [abi, arg]: any[])=> {
+      if(abi === undefined || arg === undefined) {
+        throw new Error(`Can't evaluate....`)
+      }
+      result[abi.name!] = arg;
+      return result;
+    }, {} as any
+  )
+}
+
+export function wrap(contract: StarknetContract) {
+  return new Proxy(
+    {},
+    {
+      get(_, callName) {
+        if(callName === 'address') {
+          return contract.address
+        }
+        // console.log(callName, contract.abi[callName]);
+        const abiEntry: AbiEntry = contract.abi[callName]
+        if(!abiEntry) {
+          throw new Error(`Can't evaluate: ${callName} in contract ${contract.address}`)
+        }
+        if(abiEntry.type != 'function') {
+          throw new Error(`Can't evaluate a non function: ${callName} in contract ${contract.address}`)
+        }
+        const cairoFunction: CairoFunction = abiEntry
+        return async (...args: any[]) => {
+          const callArgs = getCallArgs(cairoFunction.inputs, args)
+          const res = await contract.call(callName, callArgs)
+
+          const results = cairoFunction.outputs.reduce((result: any, {name}: AbiEntry) => {
+            result[result.length] = res[name]
+            return result
+          }, [])
+          return results.length === 1 ? results[0] : results
+        }
+      },
+    }
+  );
 }
